bb7da7267c9bc6afba6782015ea560af224097db
Magret <belloabiodun17@gmail.com>
2024-06-06 15:01:36 +0000

    Task 1 solution

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+"""A type annoted function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A concatenated function
+
+    Args:
+    str1: first argument
+    str2: second argument
+
+    Return:
+    Contenated strings
+    """
+
+    return str1 + str2
