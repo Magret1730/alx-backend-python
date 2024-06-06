1d820b09edd14189a999a6ae791fa1a4e5950b78
Magret <belloabiodun17@gmail.com>
2024-06-06 14:54:36 +0000

    Task 0 solution

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""A function """
+
+
+def add(a: float, b: float) -> float:
+    """
+    A type-annotated function
+
+    Args:
+    a: float
+    b: float
+
+    Returns:
+    the sum of the parameters in float
+    """
+
+    return a + b
