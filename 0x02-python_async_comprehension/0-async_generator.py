d5001d85a0f019f5043706fe99dc6b556ca9dda3
Magret <belloabiodun17@gmail.com>
2024-06-11 13:47:40 +0000

    Task 0 solution

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 0 solution"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Caroutine that loops 10 times each time async
+    wait 1 sec and then yield a random number
+    between 0 and 10
+    """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
