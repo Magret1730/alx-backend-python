59f15d7efb5650340ba4d753ea04450efba52d57
Magret <belloabiodun17@gmail.com>
2024-06-28 12:39:17 +0000

    Task 2 solved

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Test cases for the function 'utils.access_nested_map' """
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 import unittest
 
@@ -24,3 +25,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test for KeyError exceptions """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test cases for the function 'utils.get_json'"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test get_json with different URLs and payloads"""
+        with patch('utils.requests.get') as mocked_get:
+            # Create a mock response object with a json method
+            mocked_response = Mock()
+            mocked_response.json.return_value = test_payload
+            mocked_get.return_value = mocked_response
+
+            # Call get_json and check the result
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
+
+            # Ensure the mocked get method was called once with the test_url
+            mocked_get.assert_called_once_with(test_url)
